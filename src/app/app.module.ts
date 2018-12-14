fdad15c260b274a5b6e2c597f3ebaf05bd5fdb79
Yobani <24927714+Yobani1987@users.noreply.github.com>
2018-12-13 23:15:20 -0600

    Set up all component pages in app folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms'; // <-- here
+import { RoundProgressModule } from 'angular-svg-round-progressbar'; // <-- here
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule, // <-- here
+    RoundProgressModule // <-- and here
   ],
   providers: [],
   bootstrap: [AppComponent]
